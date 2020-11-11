d48ab464993173d544ef3b01e0932489d6733bca
XavBryant <>
2020-11-11 12:48:36 -0600

    init

---
+++ Analyses/articles/articletemplate.php
@@ -0,0 +1,50 @@
+<head>
+<link rel="stylesheet" type="text/css" href="navbar.css">
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<?php include 'nav().html';?>
+
+<h2>hi</h2>
+<p>this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    vthis is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    vthis is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    vthis is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+    vthis is textthis is textthis is textthis is textthis is textthis is text
+    this is textthis is textthis is textthis is textthis is textthis is text
+</p>
